779b9c078f69c12faa138044f5dcc6e615b328b3
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-05-24 00:39:01 +0200

    Initial commit with code examples

--- SHA1 - example.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-# https://docs.python.org/3/library/hashlib.html
-
-hash_object = hashlib.sha1(b'HelWorld')
-pbHash = hash_object.hexdigest()
-length = len(pbHash.decode("hex"))
-print(length)
