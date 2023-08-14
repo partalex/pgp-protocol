0a11d4e1873a76d3c7d82559db3b03cc75a987cd
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-14 20:30:43 +0200

    Update Compression.py

--- Aleksandar/Compression.py
+++ Aleksandar/Compression.py
@@ -8,3 +8,21 @@ print(dataCompressed)
 
 dataOriginal = zlib.decompress(dataCompressed)
 print(dataOriginal.decode())
+
+
+class Compression:
+    @staticmethod
+    def compressString(data):
+        return zlib.compress(data.encode('utf-8'))
+
+    @staticmethod
+    def compressBytes(data):
+        return zlib.compress(data)
+
+    @staticmethod
+    def decompressToString(data):
+        return zlib.decompress(data).decode('utf-8')
+
+    @staticmethod
+    def decompressToBytes(data):
+        return zlib.decompress(data)
