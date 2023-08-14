3166dead19b48a478c4194b5ddf8142c05ff3652
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-14 13:34:55 +0200

    Update Radix64 - example.com.py

--- Radix64 - example.com.py
+++ Radix64 - example.com.py
@@ -1,7 +1,43 @@
-import struct
+import base64
 
-def toRadix64(data):
-    # paziti da li je data strign ili niz bajtova
-    # https://stackoverflow.com/questions/5305456/python-how-do-i-convert-from-binary-to-base-64-and-back
-    return struct.pack('I', data).encode('base64')
 
+class Radix64:
+
+    @staticmethod
+    def encodeFromString(palintexString):
+        palintexString = palintexString.encode('ascii')
+        return Radix64.encodeBytes(palintexString)
+
+    @staticmethod
+    def encodeBytes(plaintextBytes):
+        return base64.b64encode(plaintextBytes)
+
+    @staticmethod
+    def decodeToString(cipher):
+        return base64.b64decode(cipher).decode('ascii')
+
+    @staticmethod
+    def decodeToBytes(cipher):
+        return base64.b64decode(cipher)
+
+
+print("Example with string:")
+message = "Hello Tony, I am Jarvis!"
+print("Message: " + message)
+
+encoded = Radix64.encodeFromString(message)
+print("Encoded: " + encoded.decode('ascii'))
+
+decoded = Radix64.decodeToString(encoded)
+print("Decoded: " + decoded)
+
+print("\nExample with bytes:")
+
+message = b"Hello Tony, I am Jarvis!"
+print("Message: " + message.decode('ascii'))
+
+encoded = Radix64.encodeBytes(message)
+print("Encoded: " + encoded.decode('ascii'))
+
+decoded = Radix64.decodeToBytes(encoded)
+print("Decoded: " + decoded.decode('ascii'))
