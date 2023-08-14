eebd40838536042efe1b0df9aa1f209904bb9487
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-14 20:30:13 +0200

    Update AES128.py

--- Aleksandar/AES128.py
+++ Aleksandar/AES128.py
@@ -4,17 +4,19 @@ from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
 
 
-def AES128_encrypt(message, key):
-    cipher_encrypt = AES.new(key, AES.MODE_CBC)
-    ct_bytes = cipher_encrypt.encrypt(pad(message, AES.block_size))
-    iv = b64encode(cipher_encrypt.iv).decode('utf-8')
-    ct = b64encode(ct_bytes).decode('utf-8')
-    return iv, ct
+class AES128:
+    @staticmethod
+    def encrypt(message, key):
+        cipher_encrypt = AES.new(key, AES.MODE_CBC)
+        ct_bytes = cipher_encrypt.encrypt(pad(message, AES.block_size))
+        iv = b64encode(cipher_encrypt.iv).decode('utf-8')
+        ct = b64encode(ct_bytes).decode('utf-8')
+        return iv, ct
 
-
-def AES128_decrypt(key, iv, ct):
-    iv = b64decode(iv)
-    ct = b64decode(ct)
-    cipher_decrypt = AES.new(key, AES.MODE_CBC, iv)
-    pt = unpad(cipher_decrypt.decrypt(ct), AES.block_size)
-    return pt
+    @staticmethod
+    def decrypt(key, iv, ct):
+        iv = b64decode(iv)
+        ct = b64decode(ct)
+        cipher_decrypt = AES.new(key, AES.MODE_CBC, iv)
+        pt = unpad(cipher_decrypt.decrypt(ct), AES.block_size)
+        return pt
