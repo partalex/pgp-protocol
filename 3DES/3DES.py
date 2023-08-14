3a6a3bffb2c9822b09d310bd7fe05be3d801f699
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-15 00:24:07 +0200

    Update 3DES.py

--- 3DES/3DES.py
+++ 3DES/3DES.py
@@ -1,29 +1,15 @@
 from pyDes import *
 
-key16B = "1234567_1234567_"
-key24B = "1234567_1234567_1234567_"
+if __name__ == '__main__':
+    key = "1234567_1234567_1234567_"  # can be 16B or 24B
+    iv = "12345678"
+    plaintext = "Please encrypt my data with 24B key."
 
-initVectorCBC = "12345678"
+    temp = triple_des(key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+    cipher = temp.encrypt(plaintext)
 
-plaintext16B = "Please encrypt my data with 16B key."
-plaintext24B = "Please encrypt my data with 24B key."
+    originalText24B = temp.decrypt(cipher)
+    print("Plaintext:", plaintext)
+    print("Cipher:", cipher)
+    print("Original message:", originalText24B.decode())
 
-k16B = triple_des(key16B, CBC, initVectorCBC, pad=None, padmode=PAD_PKCS5)
-k24B = triple_des(key24B, CBC, initVectorCBC, pad=None, padmode=PAD_PKCS5)
-
-ciphertext16B = k16B.encrypt(plaintext16B)
-ciphertext24B = k24B.encrypt(plaintext24B)
-
-originalText16B = k16B.decrypt(ciphertext16B)
-originalText24B = k24B.decrypt(ciphertext24B)
-
-print("16B")
-print("Plaintext message:", plaintext16B)
-print("Cipher message:", ciphertext16B)
-print("Original message:", originalText16B.decode())
-print("24B")
-print("Plaintext:", plaintext24B)
-print("Cipher:", ciphertext24B)
-print("Original message:", originalText24B.decode())
-
-# assert k16B.decrypt(ciphertext16B, padmode=PAD_PKCS5) == plaintext
