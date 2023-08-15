c7597eb53d46f934c4434a08ee5d49dd526b5cab
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-15 08:35:07 +0200

    Create FileJSON.py

---
+++ Aleksandar/FileJSON.py
@@ -0,0 +1,29 @@
+import json
+
+
+class FileJSON:
+    @staticmethod
+    def writeToFile(data, filename):
+        json_object = json.dumps(data, indent=4)
+
+        with open(filename, "w") as outfile:
+            outfile.write(json_object)
+
+    @staticmethod
+    def readFromFile(filename):
+        with open(filename, "r") as read_file:
+            data = json.load(read_file)
+        return data
+
+
+if __name__ == '__main__':
+    data = {
+        "name": "sathiyajith",
+        "rollno": 56,
+        "cgpa": 8.6,
+        "phonenumber": "9976770500"
+    }
+    filename = "data.json"
+
+    FileJSON.writeToFile(data, filename)
+    print(FileJSON.readFromFile(filename))
