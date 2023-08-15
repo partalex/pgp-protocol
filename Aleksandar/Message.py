45f63f8aeae4f231b433bc2d317592d102acf3b5
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-15 14:59:45 +0200

    Update Message.py

--- Aleksandar/Message.py
+++ Aleksandar/Message.py
@@ -12,37 +12,45 @@ class Message:
     def send(plaintext, filename, info):
         ciphertext = plaintext.encode('utf-8')
         encryptionInfo = {}
+        # print text message
+        print('0. Message:')
+        print('\t' + plaintext)
 
-        print('1. Security - which symmetric algorithm is chosen:' + info['symmetric'])
+        print('1. Security - which symmetric algorithm is chosen: ' + info['symmetric'])
         match info['symmetric']:
             case '3DES':
-                print('Message secured with 3DES encryption')
+                print('\t1.1 Message is encrypted with 3DES. ')
                 ciphertext = TripleDES.encrypt(ciphertext, bytes.fromhex(info['3DES']['key']), info['3DES']['ivCBC'])
             case 'AES128':
-                print('Message secured with AES128 encryption')
-                ciphertext = AES128.encrypt(ciphertext, bytes.fromhex(info['AES128']['key']))
+                print('\t1.1 Message is encrypted with AES128.')
+                data = AES128.encrypt(ciphertext, bytes.fromhex(info['AES128']['key']))
+                ciphertext = data['ciphertext']
+                info['AES128']['iv'] = data['iv'].decode('utf-8')
             case _:
-                print('Message not secured')
+                print('\t1.1 Message is not encrypted.')
 
         print('2. Authentication:')
         if info['wantSHA1']:
             signatureSHA1 = SHA1.sign(ciphertext)
             info['signatureSHA1'] = signatureSHA1
-            print('Message is signed with SHA1.')
+            print('\t2.1 Message is signed with SHA1.')
 
             print('3. Compression:')
             if info['wantCompression']:
                 ciphertext = Compression.compress(ciphertext)
+                print('\t3.1 Message is compressed.')
 
             print('4. Convert to radix64:')
             if info['wantRadix64']:
                 ciphertext = Radix64.encodeBytes(ciphertext)
+                print('\t4.1 Message is converted to radix64.')
 
             print('5. Save to file:')
             info['ciphertext'] = ciphertext.decode('utf-8')
             info['plaintext'] = plaintext
             encryptionInfo['info'] = info
             FileJSON.writeToFile(filename, encryptionInfo)
+            print('\t5.1 Message is saved to file.')
 
         return ciphertext
 
@@ -77,5 +85,7 @@ class Message:
                 plaintext = TripleDES.decrypt(plaintext, bytes.fromhex(info['3DES']['key']), info['3DES']['ivCBC'])
             case 'AES128':
                 print('Message received with AES128 encryption')
-                plaintext = AES128.decrypt(plaintext, bytes.fromhex(info['AES128']['key']))
+                plaintext = AES128.decrypt(plaintext, bytes.fromhex(info['AES128']['key']),
+                                           info['AES128']['iv'].encode('utf-8'))
+
         return plaintext.decode('utf-8')
