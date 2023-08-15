0398f76a6c18a9d35ccda02a6b642fea34ff0760
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-15 12:54:30 +0200

    Update SHA1.py

--- Aleksandar/SHA1.py
+++ Aleksandar/SHA1.py
@@ -4,13 +4,12 @@ import hashlib
 class SHA1:
     @staticmethod
     def signString(message):
-        hash_obj = hashlib.sha1(message.encode('utf-8'))
-        return hash_obj.hexdigest()
+        return hashlib.sha1(message.encode('utf-8')).hexdigest()
 
     @staticmethod
     def sign(message):
-        hash_obj = hashlib.sha1(message)
-        return hash_obj.hexdigest()
+        return hashlib.sha1(message).hexdigest()
+
 
     @staticmethod
     def verify(message, signature):
@@ -22,7 +21,7 @@ if __name__ == '__main__':
     message = "Hello Tony, I am Jarvis!"
     print("Message: " + message)
 
-    signature = SHA1.sign(message)
+    signature = SHA1.signString(message)
     print("Signature: " + signature)
 
     print("Verify: " + str(SHA1.verify(message, signature)))
