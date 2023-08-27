d824d39c26af02ad6869560d69a92e648a26b70b
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-27 23:49:11 +0200

    Update DSA.py

--- Aleksandar/DSA.py
+++ Aleksandar/DSA.py
@@ -41,34 +41,45 @@ class DSA:
 
     @staticmethod
     def importKey(key):
-        return CryptodomeDSA.import_key(key)
+        return CryptodomeDSA.import_key(key.encode('utf-8'))
+
+    @staticmethod
+    def exportKey(key) -> str:
+        return key.export_key().decode('utf-8')
 
 
 if __name__ == "__main__":
-    dsa = DSA(CryptodomeDSA.generate(2048), b"Hello")
-    print(dsa.verify(b"Hello", dsa.signature, dsa.key))
+    # dsa = DSA(CryptodomeDSA.generate(1024), b"Hello")
+    # print(dsa.verify(b"Hello", dsa.signature, dsa.key))
 
-    # Create a new DSA key
-    key = CryptodomeDSA.generate(2048)
-    f = open("public_key.pem", "wb")
-    f.write(key.publickey().export_key())
-    f.close()
+    key = CryptodomeDSA.generate(1024)
+    keyExported = DSA.exportKey(key)
+    print(keyExported)
 
-    # Sign a message
-    message = b"Hello"
-    hash_obj = SHA256.new(message)
-    signer = DSS.new(key, 'fips-186-3')
-    signature = signer.sign(hash_obj)
+    keyImported = DSA.importKey(keyExported)
+    print(keyImported)
 
-    # Load the public key
-    f = open("public_key.pem", "r")
-    hash_obj = SHA256.new(message)
-    pub_key = CryptodomeDSA.import_key(f.read())
-    verifier = DSS.new(pub_key, 'fips-186-3')
-
-    # Verify the authenticity of the message
-    try:
-        verifier.verify(hash_obj, signature)
-        print("The message is authentic.")
-    except ValueError:
-        print("The message is not authentic.")
+    # # Create a new DSA key
+    # key = CryptodomeDSA.generate(2048)
+    # f = open("public_key.pem", "wb")
+    # f.write(key.publickey().export_key())
+    # f.close()
+    #
+    # # Sign a message
+    # message = b"Hello"
+    # hash_obj = SHA256.new(message)
+    # signer = DSS.new(key, 'fips-186-3')
+    # signature = signer.sign(hash_obj)
+    #
+    # # Load the public key
+    # f = open("public_key.pem", "r")
+    # hash_obj = SHA256.new(message)
+    # pub_key = CryptodomeDSA.import_key(f.read())
+    # verifier = DSS.new(pub_key, 'fips-186-3')
+    #
+    # # Verify the authenticity of the message
+    # try:
+    #     verifier.verify(hash_obj, signature)
+    #     print("The message is authentic.")
+    # except ValueError:
+    #     print("The message is not authentic.")
