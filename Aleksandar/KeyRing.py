c51305711dcbaa23eb773998ccd113b4726aa506
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-27 04:12:08 +0200

    Update KeyRing.py

--- Aleksandar/KeyRing.py
+++ Aleksandar/KeyRing.py
@@ -11,23 +11,23 @@ class KeyRing:
 
     def __initialise(self):
         self.ring = FileJSON.readFromFile(self.filename)
-        for key in self.ring:
-            with open("./resources/" + key["publicKey"], 'r') as the_file:
+        for keyRing in self.ring:
+            with open("./resources/" + keyRing["publicKey"], 'r') as the_file:
                 publicKey = rsa.PublicKey.load_pkcs1(the_file.read().encode('utf8'))
-                key["publicKey"] = publicKey
-                key["timestamp"] = Timestamp.generateString()
-                key["keyId"] = key["publicKey"].n % 2 ** 32  # TODO - Check if this is correct.
+                keyRing["publicKey"] = publicKey
+                keyRing["timestamp"] = Timestamp.generateString()
+                keyRing["keyId"] = keyRing["publicKey"].n % 2 ** 32  # TODO - Check if this is correct.
 
     def getPublicKeyByKeyId(self, keyId):
-        for key in self.ring:
-            if key["keyId"] == keyId:
-                return key["publicKey"]
+        for keyRing in self.ring:
+            if keyRing["keyId"] == keyId:
+                return keyRing["publicKey"]
         raise Exception("Key not found.")
 
     def getPublicKeyByUserId(self, userId, password):
-        for key in self.ring:
-            if key["userId"] == userId and key["password"] == password:
-                return key["publicKey"]
+        for keyRing in self.ring:
+            if keyRing["userId"] == userId and keyRing["password"] == password:
+                return keyRing["publicKey"]
         raise Exception("Key not found.")
 
 
