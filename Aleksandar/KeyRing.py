de2bae2d147fe25d1e6c1106e397181a6422e6c9
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-28 18:05:03 +0200

    Update KeyRing.py

--- Aleksandar/KeyRing.py
+++ Aleksandar/KeyRing.py
@@ -43,7 +43,7 @@ class KeyRing:
             "Key Id": DSA.getKeyId(keys),
             "User Id": user_id,
             "Password": password,
-            "Type": "ElGamal"
+            "Type": "DSA"
         })
 
     def getPrivateKeyByKeyId(self, key_id):
