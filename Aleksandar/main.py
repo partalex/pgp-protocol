d793e3437221f5a9fa685c7837d8cf6f834b1a10
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-08-28 16:04:05 +0200

    Update main.py

--- Aleksandar/main.py
+++ Aleksandar/main.py
@@ -7,13 +7,15 @@ from PGPMessage import PGPMessage
 if __name__ == "__main__":
     listKeysInfo = FileManager.jsonReadFromFile("./resources/KeyRingInfo")
 
-    # Load keyring.
+    # Initialise keyring.
     keyRing = KeyRing()
     for keyInfo in listKeysInfo:
-        if keyInfo["type"] == "RSA":
-            keyRing.generateRSAKeys(keyInfo["keySize"], keyInfo["userId"], keyInfo["password"])
-        elif keyInfo["type"] == "DSA":
-            keyRing.generateDSAKeys(keyInfo["keySize"], keyInfo["userId"], keyInfo["password"])
+        if keyInfo["Type"] == "RSA":
+            keyRing.generateRSAKeys(keyInfo["Key size"], keyInfo["User Id"], keyInfo["Password"])
+        elif keyInfo["Type"] == "ElGamal":
+            keyRing.generateElGamalKeys(keyInfo["Key size"], keyInfo["User Id"], keyInfo["Password"])
+        elif keyInfo["Type"] == "DSA":
+            keyRing.generateDSAKeys(keyInfo["Key size"], keyInfo["User Id"], keyInfo["Password"])
         else:
             raise Exception("Invalid key type.")
     # keyRing.print()
@@ -24,27 +26,30 @@ if __name__ == "__main__":
     sendInfo = FileManager.jsonReadFromFile(inputInfo)
 
     # Prepare parameters.
-    message = sendInfo['message']
-    output = sendInfo['output']
-    authentication_alg = sendInfo['authentication_alg']  # [RSA, DSA, NONE].
-    encryption_alg = sendInfo['encryption_alg']  # [3DES, AES128, NONE] # Optional.
-    signature_alg = sendInfo['signature_alg']  # [RSA, ElGamal, DSA), NONE]
-    # If encryption is DSA here is ElGamal.
-    # If encryption is NONE, here NONE too.
-    # senderPrivateKey = sendInfo['senderPrivateKey']
-    # receiverPublicKey = sendInfo['receiverPublicKey']
-    savePath = sendInfo['savePath']
+    message = sendInfo['Message']
+    output = sendInfo['Output']
+    authentication_alg = sendInfo['Authentication algorithm']  # [RSA, DSA, NONE].
+    signature_alg = sendInfo['Signature algorithm']  # [RSA, ElGamal, DSA), NONE]
+    encryption_alg = sendInfo['Encryption algorithm']  # [3DES, AES128, NONE]
+    savePath = sendInfo['Save path']
 
     authentication_key = keyRing.ring[0]['Private key']  # [{RSA}, DSA, NONE]
-    encryption_key = get_random_bytes(24)  # [{3DES}, AES128, NONE]
-    signature_key = keyRing.ring[0]['Public key']  # [{RSA}, ElGamal, NONE]
+    authentication_key_id = keyRing.ring[0]['Key Id']  # [{RSA}, DSA, NONE]
+
+    signature_key = get_random_bytes(24)  # [{3DES}, AES128, NONE]
+    encryption_key = keyRing.ring[0]['Public key']  # [{RSA}, ElGamal, NONE]
 
     # Send message.
+    print("Message: " + message)
+    print("Sending message...")
     ciphertext = PGPMessage.send(
         output, message,
-        authentication_alg, encryption_alg, signature_alg,
-        authentication_key, encryption_key, signature_key,
+        authentication_alg, signature_alg, encryption_alg,
+        authentication_key, signature_key, encryption_key,
         savePath)  # TODO - Missing session key.
 
     # Receive message.
     originalMessage = PGPMessage.receive(ciphertext, keyRing)
+    print()
+    print("Received message: ")
+    print(originalMessage)
